84f1ae84162a059024b5235305146d3c109bfc6e
Qidong He <qidong.he.28@gmail.com>
2025-08-15 23:55:31 -0400

    Add files via upload

--- lattices/simple-cubic.py
+++ lattices/simple-cubic.py
@@ -1,22 +1,26 @@
 import sympy
-from typing import Tuple
+from typing import Tuple, List
 from .base import Lattice
 
 LATTICE_NAME = 'simple-cubic'
 
 def get_lattice() -> Lattice:
-    """Returns a configured simple-cubic Lattice object."""
     lattice = Lattice(
         name=LATTICE_NAME,
         dim=3,
-        neighbors=[(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)],
-        symbols=[sympy.Symbol('x'), sympy.Symbol('y'), sympy.Symbol('z')]
+        symbols=[sympy.Symbol('x'), sympy.Symbol('y'), sympy.Symbol('z')],
+        vertex_reps=[(0, 0, 0)]
     )
+
+    _NEIGHBORS = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+
+    def get_neighbor_vectors(node: Tuple[int, ...]) -> List[Tuple[int, ...]]:
+        return _NEIGHBORS
 
     def get_canonical_form(walk: Tuple[Tuple[int, ...], ...]) -> Tuple[Tuple[int, ...], ...]:
         if walk in lattice._memo_canonical: return lattice._memo_canonical[walk]
         start_node = walk[0]
-        translated = tuple((p[0]-start_node[0], p[1]-start_node[1], p[2]-start_node[2]) for p in walk)
+        translated = tuple((p[0] - start_node[0], p[1] - start_node[1], p[2] - start_node[2]) for p in walk)
         symmetries = [
             translated,
             tuple((-px, py, pz) for px, py, pz in translated),
@@ -30,12 +34,17 @@ def get_lattice() -> Lattice:
         canonical = min(symmetries)
         lattice._memo_canonical[walk] = canonical
         return canonical
-
+    
     def get_step_weight(p1: Tuple[int, ...], p2: Tuple[int, ...]) -> sympy.Symbol:
-        if p1[0] != p2[0]: return lattice.symbols[0]
-        if p1[1] != p2[1]: return lattice.symbols[1]
-        return lattice.symbols[2]
+        if p1[0] != p2[0]:
+            return lattice.symbols[0]
+        elif p1[1] != p2[1]:
+            return lattice.symbols[1]
+            # return lattice.symbols[0]  # Uncomment this line and comment the above line to weigh x and y steps the same
+        else:
+            return lattice.symbols[2]
 
+    lattice.get_neighbor_vectors = get_neighbor_vectors
     lattice.get_canonical_form = get_canonical_form
     lattice.get_step_weight = get_step_weight
     return lattice
--- lattices/triangular.py
+++ lattices/triangular.py
@@ -1,18 +1,21 @@
 import sympy
-from typing import Tuple
-from .base import Lattice  # Relative import from the 'base.py' file in the same package.
+from typing import Tuple, List
+from .base import Lattice
 
-# A unique name for this lattice, used for discovery.
 LATTICE_NAME = 'triangular'
 
 def get_lattice() -> Lattice:
-    """Returns a configured triangular Lattice object."""
     lattice = Lattice(
         name=LATTICE_NAME,
         dim=2,
-        neighbors=[(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1)],
         symbols=[sympy.Symbol('x'), sympy.Symbol('y'), sympy.Symbol('z')],
+        vertex_reps=[(0, 0)]
     )
+
+    _NEIGHBORS = [(1, 0), (0, 1), (1, 1), (-1, 0), (0, -1), (-1, -1)]
+
+    def get_neighbor_vectors(node: Tuple[int, ...]) -> List[Tuple[int, ...]]:
+        return _NEIGHBORS
 
     def get_canonical_form(walk: Tuple[Tuple[int, ...], ...]) -> Tuple[Tuple[int, ...], ...]:
         if walk in lattice._memo_canonical: return lattice._memo_canonical[walk]
@@ -25,15 +28,17 @@ def get_lattice() -> Lattice:
         canonical = min(symmetries)
         lattice._memo_canonical[walk] = canonical
         return canonical
-
+    
     def get_step_weight(p1: Tuple[int, ...], p2: Tuple[int, ...]) -> sympy.Symbol:
-        if p1[1] == p2[1]:
-            return lattice.symbols[0]
-        elif p1[0] == p2[0]:
+        if p1[0] == p2[0]:
             return lattice.symbols[1]
+            # return lattice.symbols[0]  # Uncomment this line and comment the above line to weigh x and y steps the same
+        elif p1[1] == p2[1]:
+            return lattice.symbols[0]
         else:
             return lattice.symbols[2]
 
+    lattice.get_neighbor_vectors = get_neighbor_vectors
     lattice.get_canonical_form = get_canonical_form
     lattice.get_step_weight = get_step_weight
     return lattice
